07e069ec375c6f6a8fb98e473b82b4e56dbb359a
Denis Kazakov <denis@kazakov.ru.net>
2022-10-10 00:32:11 +0300

    add basic core test & replace in-memory db to in-file

--- src/backend/tests/test_core.py
+++ src/backend/tests/test_core.py
@@ -6,17 +6,29 @@ from fastapi import status
 CORE_PREFIX = '/api/v1'
 
 
-@pytest.mark.usefixtures('user')
-@pytest.mark.parametrize('logged_in, status_code', [
-    (True, status.HTTP_200_OK),
-    (False, status.HTTP_403_FORBIDDEN),
+@pytest.mark.parametrize('path', [
+    '/tree',
+    '/tree/1',
+    '/tree/1/scheme',
+    '/tree/1/persons',
+    '/tree/1/persons/1',
+    '/tree/1/persons/1/relatives/2',
+    '/tree/1/relations',
 ])
-async def test_core_api_tree_list(client, session, logged_in, status_code):
-    token = session if logged_in else ''
+@pytest.mark.parametrize('method', ['get', 'post'])
+async def test_core_api_not_logged(client, method, path):
+    response = getattr(client, method)(CORE_PREFIX + path)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.usefixtures('user')
+async def test_core_api_tree_list(client, session):
     response = client.get(
         CORE_PREFIX + '/tree',
         cookies={
-          AUTH_COOKIE: f'Bearer {token}',
+          AUTH_COOKIE: f'Bearer {session}',
         },
     )
-    assert response.status_code == status_code
+    assert response.status_code == status.HTTP_200_OK
+    # TODO: add schema test
+    assert isinstance(response.json(), list)
