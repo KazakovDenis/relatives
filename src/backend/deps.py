cd1b5bdfe3d7309f4286e04fcc226d9c5d745658
Denis Kazakov <denis@kazakov.ru.net>
2022-11-22 00:05:26 +0300

    add jinja2.ext.i18n

--- src/backend/deps.py
+++ src/backend/deps.py
@@ -1,3 +1,5 @@
+import gettext
+
 import sqlalchemy
 from config import settings
 from databases import Database
@@ -6,4 +8,11 @@ from fastapi.templating import Jinja2Templates
 
 db = Database(settings.get_db_dsn())
 metadata = sqlalchemy.MetaData()
-templates = Jinja2Templates(settings.TEMPLATES_DIR)
+templates = Jinja2Templates(settings.TEMPLATES_DIR, extensions=['jinja2.ext.i18n'])
+templates.env.install_gettext_callables(
+    gettext=gettext.gettext,
+    ngettext=gettext.ngettext,
+    npgettext=gettext.npgettext,
+    pgettext=gettext.pgettext,
+    newstyle=True,
+)
