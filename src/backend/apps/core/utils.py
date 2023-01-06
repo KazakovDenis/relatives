682ef6ab06eb98c8ec62b13d9d2be7747c03d1c5
Denis Kazakov <denis@kazakov.ru.net>
2023-01-06 23:34:49 +0300

    remove uploading from csv

--- src/backend/apps/core/utils.py
+++ src/backend/apps/core/utils.py
@@ -1,56 +1,8 @@
-from typing import Tuple
 from uuid import UUID
 
-import aiofiles
-import pydantic
-from aiocsv import AsyncDictReader
-from ormar import NoMatch
-
 from ..auth.models import User
-from .constants import RelationType
 from .file import copy_person_photo
 from .models import Person, PersonTree, Photo, Relation, Tree, UserTree
-from .schemas import PersonSchema
-
-
-async def load_persons_csv(filename: str) -> Tuple[int, int]:
-    """Load person data from a CSV-file."""
-    loaded = errors = 0
-
-    async with aiofiles.open(filename) as f:
-        async for row in AsyncDictReader(f):
-            row['birthdate'] = row['birthdate'] or None
-            try:
-                validated = PersonSchema(**row)
-                person = await Person.objects.create(**validated.dict())
-                loaded += 1
-            except pydantic.ValidationError:
-                errors += 1
-                continue
-
-            for person_id, relation in [
-                (row['mother'], RelationType.PARENT),
-                (row['father'], RelationType.PARENT),
-                (row['spouse'], RelationType.SPOUSE),
-                *[
-                    (ex_spouse_id, RelationType.EX_SPOUSE)
-                    for ex_spouse_id in row['ex_spouse'].split(',')
-                ],
-                *[
-                   (child_id, RelationType.CHILD)
-                    for child_id in row['children'].split(',')
-                ],
-
-            ]:
-                if not person_id:
-                    continue
-                try:
-                    relative = await Person.objects.get(id=int(person_id))
-                    await person.add_relative(relation, relative)
-                except (ValueError, NoMatch):
-                    pass
-
-    return loaded, errors
 
 
 def str_to_uuid(token: str) -> UUID | None:
